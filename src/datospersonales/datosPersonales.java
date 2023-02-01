998632f5fad6cd6fe93d94f73eb6a6d14d092ed0
FranTarifa <ftarifa@americavirtualsa.com>
2023-02-01 12:46:46 -0300

    Se agrega la Tarea 1

---
+++ src/datospersonales/datosPersonales.java
@@ -0,0 +1,12 @@
+package datospersonales;
+
+public class datosPersonales {
+	public static void main (String[] args) {
+		String nombre = "Francisco";
+		String apellido = "Tarifa";
+		Integer edad = 22;
+		String lugarDeResidencia = "Buenos Aires, Haedo";
+		System.out.println(nombre + "\n" + apellido + "\n" + edad + "\n" + lugarDeResidencia);
+	}
+
+}
